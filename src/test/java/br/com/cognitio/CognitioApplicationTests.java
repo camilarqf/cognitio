f4a356890fd04047e5877ffa6919a3ded90de398
Camila Fonseca <camilarqf@gmail.com>
2024-02-01 23:42:36 -0300

    initial commit

---
+++ src/test/java/br/com/cognitio/CognitioApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cognitio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CognitioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
